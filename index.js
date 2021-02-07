d3cd2b262405337c90366a8e106e8a6d372bb683
Jacob Harvey <jacobharv00@gmail.com>
2021-02-07 01:41:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function viewCart() {
         for (let i = 0; i < cart.length-1; i++) {
           cartList.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
         }
-        return `${message} ${cartList}, and ${cart[cart.length - 3].itemName} at $${cart[cart.length - 3].itemPrice}.`
+        return `${message} ${cartList}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
       }
     }
   }
